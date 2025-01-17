b7e99f2cebaacd99daf6f73e612ea064b2e8dfdb
Paul Lakandri <paul.lakandri50@gmail.com>
2025-01-18 00:45:40 +0545

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scrollDown: {
+          "0%": {
+            transform: "translate(-50%, 0)",
+            opacity: "1",
+          },
+          "75%": {
+            transform: "translate(-50%, 16px)",
+            opacity: "0",
+          },
+          "76%, 100%": {
+            transform: "translate(-50%, 0)",
+            opacity: "0",
+          },
+        },
+      },
+      animation: {
+        scrollDown: "scrollDown 2s cubic-bezier(0.45, 0, 0.55, 1) infinite",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
