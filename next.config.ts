51071e22b72594247874c1d4fd78aa48549db5ff
Paul Lakandri <paul.lakandri50@gmail.com>
2025-01-19 16:19:51 +0545

    s

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["img.youtube.com"],
+  },
 };
 
 export default nextConfig;
